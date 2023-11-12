b7e3acac3cde2d1b04f57df20c5d179d4d4c16a8
johnx4321 <johnx4321gaming@gmail.com>
2023-11-12 14:47:49 +0530

    Update synchronous logic

--- app/src/main/java/com/example/maps/utils/CachedWebViewClient.kt
+++ app/src/main/java/com/example/maps/utils/CachedWebViewClient.kt
@@ -10,12 +10,18 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
 import java.io.OutputStream
+import java.net.HttpURLConnection
 import java.net.URL
 import java.util.LinkedList
 import java.util.Queue
 import java.util.concurrent.Executors
 
 class CachedWebViewClient(val webView: WebView): WebViewClient() {
+
+    companion object {
+        const val ENCODING = "UTF-8"
+        //for maps js api all follow this encoding. Dyanmically it can be obtained from connection.contentType. Optimized for this purpose
+    }
 
     //required for parallel
     private val networkIOExecutor = Executors.newFixedThreadPool(5)
@@ -42,7 +48,7 @@ class CachedWebViewClient(val webView: WebView): WebViewClient() {
         if (url.contains("QuotaService") && !cacheFile.exists()) return WebResourceResponse("text/javascript","UTF-8",webView.context?.assets?.open("QuotaService.js"))
         response = if (cacheFile.exists()) {
             Log.d(Utility.TAG,"Cache exists")
-            readFromCache(url, cacheFile,z)
+            readFromCache(url, cacheFile, z)
             z
         } else {
             readFromNetwork(url, cacheFile,z)
@@ -70,10 +76,21 @@ class CachedWebViewClient(val webView: WebView): WebViewClient() {
 
     private fun readFromNetwork(url: String?, cache: File,z: WrappedWebResourceResponse): WebResourceResponse? {
         //required for parallel
-       // networkIOExecutor.execute {
+        var mimeConnection: HttpURLConnection? = null
+        try {
+            mimeConnection = URL(url).openConnection() as HttpURLConnection
+            z.mimeType = mimeConnection.contentType.split(";")[0]
+            z.encoding = ENCODING
+            mimeConnection.disconnect()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            mimeConnection?.disconnect()
+        }
+        networkIOExecutor.execute {
             Log.d(Utility.TAG, "Read from network for : $url")
             //Don't use cached asset file, load the original one directly.
-            if (url?.equals("file:///android_asset/maps.html") == true || url?.contains("data:")==true) return null//@execute
+            if (url?.equals("file:///android_asset/maps.html") == true || url?.contains("data:")==true) return@execute
             var connectionInputStream: InputStream? = null
             var fileOutputStream: OutputStream? = null
             var headerOutputStream: OutputStream? = null
@@ -99,7 +116,7 @@ class CachedWebViewClient(val webView: WebView): WebViewClient() {
                         fileOutputStream.write(buffer,0,len)
                     }
                 }
-                headerOutputStream?.write(hStr.encodeToByteArray())
+                headerOutputStream?.write(hStr.encodeToByteArray());
                 //changes for synchronous
                 z.modifyResource(contentTypeArr[0],"UTF-8",cache.inputStream())
 
@@ -115,7 +132,7 @@ class CachedWebViewClient(val webView: WebView): WebViewClient() {
 
             }
         //required for parallel
-       // }
+        }
         //return null
         //changes for synchronous
         return z
